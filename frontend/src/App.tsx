580533bdf6c5c3b96af2a4d1c367b7bcdde1cd65
David Reese <93809439+eseer-divad@users.noreply.github.com>
2024-03-06 22:52:37 -0500

    Create app.tsx product page header div

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Sticky top div*/}
+      <header className="bg-white border-bottom border-black sticky-top" style={{borderWidth: '5px !important'}}>
+        <div className="container">
+          <h1 className="text-left font-weight-bold">Damask HR Concept</h1>
+        </div>
       </header>
     </div>
   );
